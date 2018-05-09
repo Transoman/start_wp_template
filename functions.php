e7705319f057810d69429d64642665941b17a887
Transoman <vasiag10@gmail.com>
2018-05-09 12:19:31 +0300

    Added function to upload SVG

--- functions.php
+++ functions.php
@@ -42,3 +42,10 @@ if ( ! function_exists( 'myfirsttheme_setup' ) ) :
 	}
 endif; // myfirsttheme_setup
 add_action( 'after_setup_theme', 'myfirsttheme_setup' );
+
+// Enable upload SVG files
+function cc_mime_types($mimes) {
+    $mimes['svg'] = 'image/svg+xml';
+    return $mimes;
+}
+add_filter('upload_mimes', 'cc_mime_types');
