d971869d2cb605cbe6c423cefbb2a330b0b53ed9
transoman <vasiag10@gmail.com>
2018-10-01 17:43:12 +0300

    Added filter and action to remove version on style and script

--- inc/remove.php
+++ inc/remove.php
@@ -11,3 +11,22 @@
   add_action( 'init', 'disable_emojis' );
 
   remove_action('wp_head', 'wp_generator');
+  add_filter('the_generator', '__return_empty_string');
+  add_filter('xmlrpc_enabled', '__return_false');
+
+
+  add_filter( 'script_loader_src', 'remove_wp_version_strings' );
+  add_filter( 'style_loader_src', 'remove_wp_version_strings' );
+  function remove_wp_version_strings( $src ) {
+    parse_str( parse_url($src, PHP_URL_QUERY), $query );
+
+    if ( !empty($query['ver']) && $query['ver'] === $GLOBALS['wp_version'] ) {
+      $src = remove_query_arg( 'ver', $src );
+    }
+
+    return $src;
+  }
+
+  add_action('wp_head',function() { ob_start(function($o) {
+    return preg_replace('/^\n?<!--.*?[Y]oast.*?-->\n?$/mi','',$o);
+  }); },~PHP_INT_MAX);
